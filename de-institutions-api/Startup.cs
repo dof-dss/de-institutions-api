74d48e09ac830643a88546bfec6d04a41a88a69f
michael.stevenson <michael.stevenson@finance-ni.gov.uk>
2021-07-31 13:00:06 +0100

    Setting up

--- de-institutions-api/Startup.cs
+++ de-institutions-api/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace de_institutions_api
 {
@@ -25,12 +20,67 @@ namespace de_institutions_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Pointer NI Addresses API",
+                    Description = "A simple address lookup for Northern Ireland addresses. Search by Post Code",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Michael Stevenson",
+                        Email = "Michael.Stevenson@finance-ni.gov.uk",
+                        Url = new Uri("https://www.micstevenson.co.uk/"),
+                    }
+                });
+
+                //c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                //{
+                //    Name = "Authorization",
+                //    Type = SecuritySchemeType.ApiKey,
+                //    Scheme = "Bearer",
+                //    BearerFormat = "JWT",
+                //    In = ParameterLocation.Header,
+                //    Description = "JWT Authorization header using the Bearer scheme."
+                //});
+
+                //c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                // {
+                //     {
+                //           new OpenApiSecurityScheme
+                //             {
+                //                 Reference = new OpenApiReference
+                //                 {
+                //                     Type = ReferenceType.SecurityScheme,
+                //                     Id = "Bearer"
+                //                 }
+                //             },
+                //             new string[] {}
+                //     }
+                // });
+
+            });
+
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
---
+++ de-institutions-infrastructure/Data/InstituitonContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace de_institutions_infrastructure.Data
+{
+    public class InstituitonContext : DbContext
+    {
+    }
+}
